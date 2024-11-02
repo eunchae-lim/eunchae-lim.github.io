c884d70e3a40c087c110f010975d2f8ce7cd3ffe
eunchae-lim <218354@jnu.ac.kr>
2024-11-02 20:29:29 +1030

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const textElement = document.getElementById('dynamic-text');
-    const texts = ["I'm a Researcher", "I'm an AI Developer"];
+    const texts = ["a Researcher", "an AI Developer"];
     let textIndex = 0;
+    let charIndex = 0;
 
     function typeText() {
-        // 텍스트 초기화
-        textElement.style.width = '0'; // 타이핑 효과를 위한 너비 초기화
-        textElement.classList.remove('typing'); // 클래스 제거로 애니메이션 초기화
-        void textElement.offsetWidth; // 리플로우 발생시켜 애니메이션 재적용
-        textElement.classList.add('typing'); // 클래스 재적용으로 애니메이션 실행
-        textElement.textContent = texts[textIndex]; // 텍스트 업데이트
-
-        // 텍스트 인덱스 업데이트
-        textIndex = (textIndex + 1) % texts.length;
+        if (charIndex < texts[textIndex].length) {
+            textElement.textContent += texts[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(typeText, 100); // 다음 글자를 타이핑하는 속도 조절
+        } else {
+            // 타이핑이 끝나면 잠시 멈춤 후 다음 텍스트로 전환
+            setTimeout(() => {
+                textElement.textContent = '';
+                charIndex = 0;
+                textIndex = (textIndex + 1) % texts.length;
+                typeText();
+            }, 2000); // 2초 대기 후 텍스트 초기화
+        }
     }
 
-    // 첫 타이핑 애니메이션 실행 및 4초마다 반복
+    // 초기 타이핑 애니메이션 시작
     typeText();
-    setInterval(typeText, 4000);
 });
